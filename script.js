0acdc7115e2f241959c7e321afce8761e4500177
fataranto <fataranto@gmail.com>
2022-05-24 17:53:00 +0200

    cálculo ok por consola

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Implementar el código aqui
 
 // Escuchar el evento 'submit'
+
+
+let calculate = document.querySelector("form");
+calculate.addEventListener("submit", function(e){
+    e.preventDefault();
+    let height = document.querySelector("#height").value;
+    let weight = document.querySelector("#weight").value;
+    let imc = (weight / ((height*height)/10000)).toFixed(2);
+    console.log(imc);
+}) 
